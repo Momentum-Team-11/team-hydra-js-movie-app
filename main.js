f85588f8548d0c3ab3c34bbb20b684ce3b5ed900
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2022-02-16 11:47:35 -0500

    delete button working

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const url = "http://localhost:3000/movies";
 const form = document.getElementById("movie-input");
 const list = document.getElementById("movie-list");
@@ -35,6 +37,24 @@ function renderMovieItem(movie) {
   const formEl = document.createElement("li");
   formEl.id = movie.id;
   formEl.classList.add("added");
-  formEl.innerHTML = `<span>${movie.title}</span><i alt="Delete Movie" class="delete"></i><i alt="Edit Movie" class="Edit"></i>`;
+  formEl.innerHTML = `<span>${movie.title}</span><i class="ml2 dark-red fas fa-times delete"></i><i class="ml3 fas fa-edit edit"></i>`;
   list.append(formEl);
 }
+
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+  deleteMovie(e.target)
+  }
+  if (e.target.classList.contains('edit')) {
+    editMovie(e.target)
+  }
+})
+
+function deleteMovie (element) {
+  const movieId = element.parentElement.id
+  fetch(url + '/' + movieId, {
+    method: 'DELETE',
+  }) .then (function () {
+    element.parentElement.remove()
+  }
+  )}
